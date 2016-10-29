9badade0bd135f83a3cf8376649024d23cafda7e
ojas <ojasok@gmail.com>
2016-10-28 23:50:23 -0600

    Accepted / Rejectedd and new

--- routes/index.js
+++ routes/index.js
@@ -38,13 +38,119 @@ router.get('/applicants', function(req, res, next) {
         resApp.push(results[a]);
       }
       res.json(resApp);
-     // console.log(ret);
-     // console.log(typeof ret[0])
-     /*console.log(results.length);
-      console.log(typeof results[0])
-      console.log(results[0].name)
-      console.log(results[0].lname)
-      console.log(results[0].telephone)*/
+
+    });
+
+  });
+
+});
+
+
+router.get('/applicants/accepted', function(req, res, next) {
+
+  var connection = mysql.createConnection({
+    host: 'malisamaj1.crwqdxb9dkgh.us-west-2.rds.amazonaws.com',
+    user: 'malisamaj',
+    password: 'malisamaj',
+    database: 'ojas'
+  })
+  //var obj=[{name:'ojas'}];
+  var ret = [];
+  connection.connect(function(err) {
+    if (err) {
+      res.send(err);
+      throw err
+    }
+
+    console.log('You are now connected...');
+
+    connection.query('SELECT * FROM applicant where status=1', function(err, results, fields) {
+
+      if (err) {
+        res.send(err);
+        throw err
+      }
+      var resApp = [];
+      for(var a=0;a<results.length;a++){
+        //console.log(JSON.stringify(results[a]))
+        resApp.push(results[a]);
+      }
+      res.json(resApp);
+
+    });
+
+  });
+
+});
+
+router.get('/applicants/rejected', function(req, res, next) {
+
+  var connection = mysql.createConnection({
+    host: 'malisamaj1.crwqdxb9dkgh.us-west-2.rds.amazonaws.com',
+    user: 'malisamaj',
+    password: 'malisamaj',
+    database: 'ojas'
+  })
+  //var obj=[{name:'ojas'}];
+  var ret = [];
+  connection.connect(function(err) {
+    if (err) {
+      res.send(err);
+      throw err
+    }
+
+    console.log('You are now connected...');
+
+    connection.query('SELECT * FROM applicant where status=-1', function(err, results, fields) {
+
+      if (err) {
+        res.send(err);
+        throw err
+      }
+      var resApp = [];
+      for(var a=0;a<results.length;a++){
+        //console.log(JSON.stringify(results[a]))
+        resApp.push(results[a]);
+      }
+      res.json(resApp);
+
+    });
+
+  });
+
+});
+
+router.get('/applicants/new', function(req, res, next) {
+
+  var connection = mysql.createConnection({
+    host: 'malisamaj1.crwqdxb9dkgh.us-west-2.rds.amazonaws.com',
+    user: 'malisamaj',
+    password: 'malisamaj',
+    database: 'ojas'
+  })
+  //var obj=[{name:'ojas'}];
+  var ret = [];
+  connection.connect(function(err) {
+    if (err) {
+      res.send(err);
+      throw err
+    }
+
+    console.log('You are now connected...');
+
+    connection.query('SELECT * FROM applicant where status=0', function(err, results, fields) {
+
+      if (err) {
+        res.send(err);
+        throw err
+      }
+      var resApp = [];
+      for(var a=0;a<results.length;a++){
+        //console.log(JSON.stringify(results[a]))
+        resApp.push(results[a]);
+      }
+      res.json(resApp);
+
     });
 
   });
